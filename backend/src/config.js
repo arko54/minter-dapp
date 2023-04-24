ed607604bacb57b338174a86e0c9d63b9a411c08
arko54 <arijan.lulevski@outlook.com>
2023-04-24 21:34:13 +0200

    testing

--- backend/src/config.js
+++ backend/src/config.js
@@ -58,7 +58,7 @@ const MINT_PRICE = 0.001; // Minting price per NFT. Goerli = ETH, Ethereum = ETH
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2023-04-25T20:00:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2023-04-30T20:00:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE = "2023-04-23T16:00:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00

2db72b1c802a3d42ffa09b17186a9bf73a3aaa48
arko54 <arijan.lulevski@outlook.com>
2023-04-24 14:56:06 +0200

    goerli test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; // Replace with your own contract address
+const contractAddress = "0x89dD847B893B54e992a2B63ce7527CC16177577D"; // Replace with your own contract address
 const chain = 'goerli'; // goerli, polygon, or ethereum
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";

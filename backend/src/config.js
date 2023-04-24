ffcdfa31561e70cbdca0b7371fde1e4efc6fe8e1
arko54 <arijan.lulevski@outlook.com>
2023-04-24 21:20:14 +0200

    presale_date

--- backend/src/config.js
+++ backend/src/config.js
@@ -37,7 +37,7 @@ const format = {
 };
 
 const extraMetadata = {
-  external_url: "https://blockchainbombers.xyz", // Replace with your website or remove this line if you do not have one.
+  external_url: "https://blockchainbombersweb.xyz", // Replace with your website or remove this line if you do not have one.
 };
 
 // NFTPort Info
@@ -61,12 +61,12 @@ const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a singl
 const PUBLIC_MINT_START_DATE = "2023-04-25T20:00:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2023-04-24T16:00:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2023-04-23T16:00:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x566c1eEcfc3E94E785a1f9f0Aa1FebC93eC5AcF4"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = ["0x566c1eEcfc3E94E785a1f9f0Aa1FebC93eC5AcF4"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it

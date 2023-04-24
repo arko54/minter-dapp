1d0b565d0437d64beb85054fc9fc51f859571870
arko54 <arijan.lulevski@outlook.com>
2023-04-24 22:37:21 +0200

    TEST...

--- backend/src/config.js
+++ backend/src/config.js
@@ -69,7 +69,7 @@ const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the
 const PRESALE_WHITELISTED_ADDRESSES = ["0x566c1eEcfc3E94E785a1f9f0Aa1FebC93eC5AcF4"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
+let CONTRACT_ADDRESS = "0x89dD847B893B54e992a2B63ce7527CC16177577D"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
